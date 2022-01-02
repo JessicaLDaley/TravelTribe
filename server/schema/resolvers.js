7c11e0c4e6cbfbaa3a4ef2b9c4c279ef5ac820c0
e1m3m0 <gamateo@gmail.com>
2022-01-02 18:16:51 -0500

    resolvers setup

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Trip } = require('../models')
 const { AuthenticationError } = require('apollo-server-express')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
@@ -34,7 +35,97 @@ const resolvers = {
       return Trip.findOne({ _id });
     }
   },
-  Mutation: {},
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args)
+      const token = signToken(user)
+
+      return { user }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials')
+      }
+
+      const correctPw = await user.isCorrectPassword(password)
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials')
+      }
+
+      const token = signToken(user)
+      return { user }
+    },
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const trip = await Trip.create({ ...args, username: context.user.username });
+    
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { trips: trip._id } },
+          { new: true }
+        );
+    
+        return trip;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addCompanion: async (parent, { userId, tripId }, context) => {
+      if (context.user) {
+        const updatedTrip = await Trip.findOneAndUpdate(
+          { _id: tripId },
+          { $addToSet: { tripCompanions: userId } },
+          { new: true }
+        ).populate('tripCompanions');
+    
+        return updatedTrip;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addComment: async (parent, { tripId, commentText }, context) => {
+      if (context.user) {
+        const updatedTrip = await Trip.findOneAndUpdate(
+          { _id: tripId },
+          { $push: { tripComments: { commentText, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+    
+        return updatedTrip;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addPlaces: async (parent, { tripId, args }, context ) => {
+      if(context.user) {
+        const updatedTrip = await Trip.findOneAndUpdate(
+          {_id: tripId},
+          { $push: { placesToSee: { args }}},
+          { new: true, runValidators: true }
+        )
+
+        return updatedTrip
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addPicture: async ( parent, { tripId, tripName, pictureId }, context ) => {
+      if(context.user) {
+        const updatedTrip = await Trip.findOneAndUpdate(
+          { _id: tripId },
+          { $push: { pictureAlbum: { tripName, pictureId , username: context.user.username }}},
+          { new: true, runValidators: true }
+        )
+
+        return updatedTrip
+      }
+
+      throw new AuthenticationError('You need to be logged in!')
+    }
+  },
 }
 
 module.exports = resolvers
