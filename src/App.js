f2506a27fe57d40daf843d0c93c282e102704a4f
JessicaLDaley <jessicadaleypaints@yahoo.com>
2022-01-02 13:43:48 -0500

    re-formatting chakra ui

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react'
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  CSSReset
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+//import Nav from "./components/Nav";
+//import Signup from "./Pages/Signup";
+//import Login from "./Pages/Login";
+//import Dashboard from "./Pages/Dashboard";
 
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <CSSReset/>
-      <Heading as="h1">
-        Travel-App
-      </Heading>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-           
-            <Text>
-             
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
+    <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </div>
+  </Router>
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+//import './index.css';
 import App from './App';
+//import Auth from "../../utils/auth";
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
+
+const colors = {
+  brand: {
+    50: '#ecefff',
+    100: '#cbceeb',
+    200: '#a9aed6',
+    300: '#888ec5',
+    400: '#666db3',
+    500: '#4d5499',
+    600: '#3c4178',
+    700: '#2a2f57',
+    800: '#181c37',
+    900: '#080819',
+  },
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ colors, config });
+
+
 ReactDOM.render(
-  <StrictMode>
-    <ColorModeScript initialColorMode="light"></ColorModeScript>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+  <ChakraProvider theme={theme}>
+   
+      <App />
+   
+  </ChakraProvider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
