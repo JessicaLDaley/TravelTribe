f067b3562cde35d64ad75fdbee0de184cb85ee4d
Steve Bendrick <steven.bendrick@gmail.com>
2022-01-03 13:34:30 -0500

    edited comments

--- server/utils/idb.js
+++ server/utils/idb.js
@@ -1,49 +1,48 @@
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
-    // open connection to the database `shop-shop` with the version of 1
+    // open connection to the database
     const request = window.indexedDB.open('trip_tribe', 1);
 
     // create variables to hold reference to the database, transaction (tx), and object store
     let db, tx, store;
 
-    // if version has changed (or if this is the first time using the database), run this method and create the three object stores
+    // if version has changed or first time running
     request.onupgradeneeded = function(e) {
       const db = request.result;
 
-      // create object store for each type of data and set "primary" key index to be the `_id` of the data
+      // create object store for each type of data (storeName) and set "primary" key index to be the `_id` of the data
       db.createObjectStore('trip', { keyPath: '_id' });
     };
 
-    // handle any errors with connecting
     request.onerror = function(e) {
       console.log('There was an error');
     };
 
-    // on database open success
     request.onsuccess = function(e) {
-      // save a reference of the database to the `db` variable
       db = request.result;
-      // open a transaction, do whatever we pass into `storeName` (must match one of the object store names)
+      // open a transaction, `readWrite` in the chosen store (must match one of the object store names)
       tx = db.transaction(storeName, 'readwrite');
       // save a reference to that object store
       store = tx.objectStore(storeName);
 
-      // if there's any errors, let us know
       db.onerror = function(e) {
         console.log('error', e);
       };
 
       switch (method) {
+        // add the trip to indexedDB when logged in or on trip creation/update
         case 'put':
           store.put(object);
           resolve(object);
           break;
+        // get the trip from indexedDB if offline or while loading
         case 'get':
           const all = store.getAll();
           all.onsuccess = function() {
             resolve(all.result);
           };
           break;
+        // Delete the trip on log-out or user request
         case 'delete':
           store.delete(object._id);
           break;
@@ -52,7 +51,6 @@ export function idbPromise(storeName, method, object) {
           break;
       }
       
-      // when the transaction is complete, close the connection
       tx.oncomplete = function() {
         db.close();
       };
