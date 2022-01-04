a794e6ebcf4b634d56004bb964c5cbe4e69bf6b4
Steve Bendrick <steven.bendrick@gmail.com>
2022-01-04 15:02:28 -0500

    added user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,23 @@
 import { gql } from "@apollo/client";
 
+// this is a basic user query, there are a lot more fields available for query
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      tripCount
+      trips {
+        _id
+        tripName
+        tripDestination
+      }
+      companionCount
+      companions {
+        _id
+        username
+      }
+    }
+  }
+`
