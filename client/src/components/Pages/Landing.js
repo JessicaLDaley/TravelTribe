ed0387facf494256300aabb7a7b87209e352ceee
JessicaLDaley <jessicadaleypaints@yahoo.com>
2022-01-03 21:33:59 -0500

    added a responsive home page with chakra and dynamic image

--- client/src/components/Pages/Landing.js
+++ client/src/components/Pages/Landing.js
@@ -7,7 +7,7 @@ export default function Landing() {
     <LandingLayout>
       <Home
         title="Travel Tribe"
-        subtitle="Organizing a travel group is sometimes challenging. Careful planning and open communication are the keys to success. Travel Tribe is an exclusive social media application that allows group members to meet, share ideas and make plans. Post schedule updates, meeting locations and share photos to commemorate your travels. Wherever the journey takes you, Travel Tribe has your back. "
+        subtitle="Organizing a travel group is sometimes challenging. Careful planning and open communication are the keys to success. Travel Tribe is an exclusive social media application that allows group members to meet, share ideas, and ultimately take the stress out of organizing. Post schedule updates, meeting locations and share photos to commemorate your travels. Wherever the journey takes you, Travel Tribe has your back. Bon voyage! "
         image= "https://source.unsplash.com/collection/959652/800x600"
         ctaText="Create your account now"
         ctaLink="/signup"
