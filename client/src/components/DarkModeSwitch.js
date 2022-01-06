3b0ebc9bc2b979ef20c3a5b32f65e81598f90926
JessicaLDaley <jessicadaleypaints@yahoo.com>
2022-01-06 10:26:46 -0500

    created data table for dashboard but still need to fill table with tripForm data

--- client/src/components/DarkModeSwitch.js
+++ client/src/components/DarkModeSwitch.js
@@ -1,25 +1,25 @@
-import { useColorMode, Switch, Flex, Button, IconButton } from '@chakra-ui/react'
-import {useState} from 'react'
-import {HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
-import NextLInk from 'next/link'
+// import { useColorMode, Switch, Flex, Button, IconButton } from '@chakra-ui/react'
+// import {useState} from 'react'
+// import {HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+// import NextLInk from 'next/link'
 
-export const DarkModeSwitch = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const isDark = colorMode === 'dark'
-  return (
-      <Flex>
-          <Flex position="fixed"
-      top="1rem"
-      right="1rem"
-      >
-      </Flex>
-      <Flex>
+// export const DarkModeSwitch = () => {
+//   const { colorMode, toggleColorMode } = useColorMode()
+//   const isDark = colorMode === 'dark'
+//   return (
+//       <Flex>
+//           <Flex position="fixed"
+//       top="1rem"
+//       right="1rem"
+//       >
+//       </Flex>
+//       <Flex>
     
-    <Switch
+//     <Switch
      
-      color="green"
-      isChecked={isDark}
-      onChange={toggleColorMode}
-    />
-  )
-}
+//       color="green"
+//       isChecked={isDark}
+//       onChange={toggleColorMode}
+//     />
+//   )
+// }
--- client/src/components/Pages/Dashboard.js
+++ client/src/components/Pages/Dashboard.js
@@ -2,25 +2,48 @@ import React from "react";
 import TripModal from "../TripForm";
 import UpcomingTrips from "../UpcomingTrips";
 import FriendsList from "../FriendsList";
-import {Flex} from '@chakra-ui/react';
-import {useQuery} from '@apollo/client';
-import {QUERY_ME} from '../../utils/queries';
+import {Flex, Table,Thead,Tbody,Tr, Th,Td,} from '@chakra-ui/react';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../../utils/queries';
+
 
 function Dashboard() {
   document.title = 'Dashboard';
 
-  const {loading, error, data} = useQuery(QUERY_ME);
+  const { loading, error, data } = useQuery(QUERY_ME);
   const user = data?.me;
 
   return (
     <Flex direction="column">
-      <TripModal/>
-      {loading? (
-        <div/>
+      <TripModal />
+      {loading ? (
+        <div />
       ) : (
         <Flex>
-          <UpcomingTrips trips={user.trips}/>
-          <FriendsList user={user}/>
+          <UpcomingTrips trips={user.trips} />
+
+          {/* Table Creation */}
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>Trip Name</Th>
+                <Th>Description</Th>
+                <Th>Destination</Th>
+                <Th isNumeric>Start Date - End Date</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td isNumeric></Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          {/* End Table */}
+          <FriendsList user={user} />
         </Flex>
       )}
     </Flex>
