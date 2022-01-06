e1402b3cd363037168dbb51ed6a52ebfcefb7098
trippjoe <tripp.j@hotmail.com>
2022-01-06 18:58:36 -0500

    updated the color of the create trip button on the modal

--- client/src/components/TripForm/TripModal.js
+++ client/src/components/TripForm/TripModal.js
@@ -89,8 +89,7 @@ function TripModal({friends}){
                     />
                 </ModalBody>
                 <ModalFooter>
-                    <Button onClick={handleFormSubmit}>Create Trip</Button>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>Close</Button>
+                    <Button colorScheme="blue" onClick={handleFormSubmit}>Create Trip</Button>
                 </ModalFooter>
             </ModalContent>
         </Modal>
