253c0451c3f5c1e565e1a2f0a692f48b6bb6da6c
JessicaLDaley <jessicadaleypaints@yahoo.com>
2022-01-06 12:31:24 -0500

    Got upcoming trip data into table and properly aligned

--- client/src/components/UpcomingTrips.js
+++ client/src/components/UpcomingTrips.js
@@ -1,20 +1,47 @@
 import React from "react";
-import {Flex} from '@chakra-ui/react';
-import {Link} from 'react-router-dom';
+import { Flex } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+} from '@chakra-ui/react'
 
-function UpcomingTrips({trips}){
-    return(
-        <Flex display="column">
-            {trips.map(trip => (
+function UpcomingTrips({ trips }) {
+    return (
+        <Table variant='simple'>
+           {trips.map(trip => (
                 <div key={trip._id}>
-                    <Link to={`./trip/${trip._id}`}>{trip.tripName}</Link>
-                    <p>{trip.tripDetails}</p>
-                    <p>{trip.tripDestination}</p>
-                    <p>{trip.tripDeparture}</p>
-                    <p>{trip.tripReturn}</p>
+            <Tr>
+                <Th>Trip Name</Th>
+                <Th>Trip Details</Th>
+                <Th>Trip Destination</Th>
+                <Th>Trip Departure</Th>
+                <Th>Trip Return</Th>
+                <Th>Trip Companions</Th>
+                
+            </Tr>
+         
+           
+                   
+
+                    <Tbody>
+<Tr>
+    <Td> <Link to={`./trip/${trip._id}`}>{trip.tripName}</Link></Td>
+<Td>{trip.tripDetails}</Td>
+<Td>{trip.tripDestination}</Td>
+<Td>{trip.tripDeparture}</Td>
+<Td>{trip.tripReturn}</Td>
+</Tr>
+</Tbody>
                 </div>
             ))}
-        </Flex>
+        </Table>
     );
 }
 
